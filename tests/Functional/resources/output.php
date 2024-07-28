9ce6fcf9d0859e22a7b5b9eabcb56e95597b1847
SerafimArts <nesk@xakep.ru>
2024-07-28 13:23:37 +0000

    Add PHP 8.4 tests support

--- tests/Functional/resources/output.php
+++ tests/Functional/resources/output.php
@@ -2,8 +2,6 @@
 
 declare(strict_types=1);
 
-use Phplrt\Parser\Parser;
-
 /**
  * @var array{
  *     initial: array-key,
@@ -31,18 +29,18 @@ return [
     ],
     'transitions' => [],
     'grammar' => [
-        new \Phplrt\Parser\Grammar\Concatenation([4, 5]),
-        new \Phplrt\Parser\Grammar\Lexeme('d', true),
-        new \Phplrt\Parser\Grammar\Repetition(0, 1, INF),
-        new \Phplrt\Parser\Grammar\Concatenation([1, 2]),
-        new \Phplrt\Parser\Grammar\Lexeme('p', false),
-        new \Phplrt\Parser\Grammar\Lexeme('d', true),
+        new Phplrt\Parser\Grammar\Concatenation([4, 5]),
+        new Phplrt\Parser\Grammar\Lexeme('d', true),
+        new Phplrt\Parser\Grammar\Repetition(0, 1, INF),
+        new Phplrt\Parser\Grammar\Concatenation([1, 2]),
+        new Phplrt\Parser\Grammar\Lexeme('p', false),
+        new Phplrt\Parser\Grammar\Lexeme('d', true),
     ],
     'reducers' => [
-        0 => static function (\Phplrt\Parser\Context $ctx, $children): void {
+        0 => static function (Phplrt\Parser\Context $ctx, $children): void {
             dump($children);
         },
-        3 => static function (\Phplrt\Parser\Context $ctx, $children) {
+        3 => static function (Phplrt\Parser\Context $ctx, $children) {
             // The "$offset" variable is an auto-generated
             $offset = $ctx->lastProcessedToken->getOffset();
 
